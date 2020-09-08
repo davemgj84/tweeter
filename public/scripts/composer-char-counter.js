9519f0a9808c808b50ffb0a18fb80d2d8dfb04f5
davemgj84 <davemgj@gmail.com>
2020-09-08 21:46:05 +0000

    implemented character counter in separate js file. when count goes below 0, characters are red, when positive default color.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,20 @@
+// Implementation of character counter - event listener:
+
+/* global document */
+/* eslint-env jquery */
+
+$(document).ready(function() {
+
+  const $textarea = $("#tweet-text");
+  $textarea.on("input", function(e) {
+    const count = 140;
+    let remainingChar = count - ($(this).val()).length;
+    $(this).siblings("div").children(".counter").val(remainingChar);
+    if (remainingChar < 0) {
+      $(this).siblings("div").children(".counter").addClass("negative");
+    } else if (remainingChar >= 0) {
+      $(this).siblings("div").children(".counter").removeClass("negative");
+    }
+  });
+
+});
