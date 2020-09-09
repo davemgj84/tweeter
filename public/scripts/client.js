fa37fff7f77c3dd320087ffa94d758475fc14f66
davemgj84 <davemgj@gmail.com>
2020-09-09 23:13:17 +0000

    implemented AJAX POST REQUEST handling and AJAX GET REQUEST handling - no longer using hardcoded user tweet database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,33 +9,6 @@
 
 $(document).ready(function() {
 
-  // Temp database for Users tweets
-  const data = [
-    {
-      "user": {
-        "name": "Jeffrey Lebowski",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@Dude"
-      },
-      "content": {
-        "text": "This is a very complicated case... You know, a lotta ins, a lotta outs, lotta what-have-yous.."
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Walter Sobchak",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@Walter"
-      },
-      "content": {
-        "text": "This is not Nam, this is bowling, there are rules.."
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
   // Creates a tweet element with a tweet template using template literals to pull data from user objects:
   const createTweetElement = function(data) {
     const tweetTemplate = `<article class="tweet" >
@@ -65,13 +38,32 @@ $(document).ready(function() {
   // renders the tweets by looping through the user tweet database and creates a tweet element for each tweet,
   // the prepends them to the appropriate section.
   const renderTweets = function(data) {
-    // loops through tweets
     for (const id of data) {
       const $tweet = createTweetElement(id);
       $('#tweets').prepend($tweet);
     }
   };
 
-  renderTweets(data);
+  // AJAX GET REQ HANDLING:
+  const loadtweets = () => {
+    $.get('/tweets', null, (posts) => {
+      renderTweets(posts);
+    });
+  };
+
+  // AJAX POST REQ HANDLING:
+  const $postForm = $('#submit-form');
+
+  $postForm.on('submit', function(event) {
+    event.preventDefault();
+    const serializedData = $(this).serialize();
+    $.post('/tweets', serializedData)
+      .then(() => {
+        loadtweets();
+        $(this).children('textarea').val('');
+        $('.counter').val(140);
+        $('.counter').removeClass('negative');
+      });
+  });
 
 });
