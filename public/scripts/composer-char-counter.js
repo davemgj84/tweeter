12822565bc6b7c53596ad0c0f142fd8d8c03e8e6
davemgj84 <davemgj@gmail.com>
2020-09-09 02:56:55 +0000

    created tweet template container - this will be a template for any of the future tweets - it also has a hover effect, showing a shadowbox, and a username when mousing over ithe container- all the styling is complete

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -6,9 +6,9 @@
 $(document).ready(function() {
 
   const $textarea = $("#tweet-text");
-  $textarea.on("input", function(e) {
+  $textarea.on("input", function(event) {
     const count = 140;
-    let remainingChar = count - ($(this).val()).length;
+    let remainingChar = count - $(this).val().length;
     $(this).siblings("div").children(".counter").val(remainingChar);
     if (remainingChar < 0) {
       $(this).siblings("div").children(".counter").addClass("negative");
